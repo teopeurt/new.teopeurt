b6c6cab73ded3ce6b7b465b69846519fb72382dd
Don Onwunumah <me@teopeurt.com>
2023-10-17 01:49:50 +0200

    buld issue fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 
 import { Space_Grotesk } from 'next/font/google'
-// import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
