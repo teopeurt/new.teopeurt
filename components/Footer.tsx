4fbd43e314a181b3b4a412afbf2d4a0550a5db4e
Don Onwunumah <me@teopeurt.com>
2024-01-09 01:16:04 +0100

    tracking and blogs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,9 +22,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/teopeurt/new.teopeurt">
-            Tailwind Nextjs Theme
-          </Link>
+          <Link href="https://github.com/teopeurt/new.teopeurt"></Link>
         </div>
       </div>
     </footer>
